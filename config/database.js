8ece20627144ac12debaac870e86a5e5d8e49b85
user <user@HP>
2021-07-17 21:23:54 +0530

    Changes to my-project noted

--- config/database.js
+++ config/database.js
@@ -4,17 +4,20 @@ module.exports = ({ env }) => ({
     default: {
       connector: 'bookshelf',
       settings: {
-       client: 'postgres',
-        host: env('DATABASE_HOST', 'localhost'),
-        port: env.int('DATABASE_PORT', 5432),
-        database: env('DATABASE_NAME', 'db_name'),
-        username: env('DATABASE_USERNAME', 'db_user'),
-        password: env('DATABASE_PASSWORD', 'password'),
-        schema: 'public',
+        client: 'postgres',
+        host: 'ec2-52-2-118-38.compute-1.amazonaws.com',
+        port: '5432',
+        database: 'd8ne79odo9dlad',
+        username: 'ujaklzdvpmayto',
+        password: '1cdfd72cee19f1b5944368f6bd8de86f232b7952b4439daecc321f6ceba48ae8',
+        ssl: {
+          rejectUnauthorized: false,
+        },
       },
       options: {
-        useNullAsDefault: true,
+        ssl: true,
       },
     },
   },
 });
+
--- config/env/production/database.js
+++ config/env/production/database.js
@@ -8,11 +8,11 @@ module.exports = ({ env }) => ({
       connector: 'bookshelf',
       settings: {
         client: 'postgres',
-        host: config.host,
-        port: config.port,
-        database: config.database,
-        username: config.user,
-        password: config.password,
+        host: 'ec2-52-2-118-38.compute-1.amazonaws.com',
+        port: '5432',
+        database: 'd8ne79odo9dlad',
+        username: 'ujaklzdvpmayto',
+        password: '1cdfd72cee19f1b5944368f6bd8de86f232b7952b4439daecc321f6ceba48ae8',
         ssl: {
           rejectUnauthorized: false,
         },
