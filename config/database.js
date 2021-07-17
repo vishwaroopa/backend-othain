5c0e33237b7145a964d059e84d933ad154f3fce5
user <user@HP>
2021-07-17 19:24:03 +0530

    Initial commit

--- config/database.js
+++ config/database.js
@@ -4,8 +4,13 @@ module.exports = ({ env }) => ({
     default: {
       connector: 'bookshelf',
       settings: {
-        client: 'sqlite',
-        filename: env('DATABASE_FILENAME', '.tmp/data.db'),
+       client: 'postgres',
+        host: env('DATABASE_HOST', 'localhost'),
+        port: env.int('DATABASE_PORT', 5432),
+        database: env('DATABASE_NAME', 'db_name'),
+        username: env('DATABASE_USERNAME', 'db_user'),
+        password: env('DATABASE_PASSWORD', 'password'),
+        schema: 'public',
       },
       options: {
         useNullAsDefault: true,
