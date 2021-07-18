7499758dc7bd8555ba508b8f76afd77065b11f06
user <user@HP>
2021-07-18 21:55:39 +0530

    latest commit

---
+++ config/plugins.js
@@ -0,0 +1,10 @@
+module.exports = ({ env }) => ({
+  upload: {
+    provider: 'cloudinary',
+    providerOptions: {
+      cloud_name: 'mavin-infotech',
+      api_key: '114681481184373',
+      api_secret: 'uVARgGpdmIUc9wBTQviLJZEKr0M',
+    },
+  },
+});
