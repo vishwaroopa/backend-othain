3eabb033dc565045e8c3cc14141b0abdfca1ce78
user <user@HP>
2021-10-19 11:41:28 +0530

    make it better

--- config/server.js
+++ config/server.js
@@ -1,6 +1,6 @@
 module.exports = ({ env }) => ({
   host: env('HOST', '0.0.0.0'),
-  port: env.int('PORT', 1337),
+  port: env.int('PORT', 1338),
   admin: {
     auth: {
       secret: env('ADMIN_JWT_SECRET', '570e1cccf3882fc9eedfc8cd062d5151'),
